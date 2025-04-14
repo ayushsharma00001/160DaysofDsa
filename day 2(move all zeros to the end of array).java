bf461364a68f32bcc72ed0ba5a763ce2aec409c9
Ayush Sharma <116947667+ayushsharma00001@users.noreply.github.com>
2025-04-14 15:24:36 +0530

    Day 2

--- day 2(move all zeros to the end of array).java
+++ day 2(move all zeros to the end of array).java
@@ -1,10 +1,10 @@
 class Solution {
     void pushZerosToEnd(int[] arr) {
         int pos = -1;
-        for(int i=0 ; i<arr.length ; i++){
-            if(arr[i]==0 && pos== -1)
+        for(int i = 0 ; i<arr.length ; i++){
+            if(pos==-1 && arr[i]==0)
                 pos = i;
-            else if(pos!=-1 && arr[pos]==0 && arr[i]!=0){
+            else if(pos != -1 && arr[i]!=0){
                 arr[i] = arr[i]+arr[pos] - (arr[pos] = arr[i]);
                 pos++;
             }
