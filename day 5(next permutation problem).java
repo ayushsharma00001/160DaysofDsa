d35e8b2fd7799b8fc7f79f658aaa9e1d259783ea
Ayush Sharma <116947667+ayushsharma00001@users.noreply.github.com>
2025-04-16 15:29:23 +0530

    Day 5

---
+++ day 5(next permutation problem).java
@@ -0,0 +1,25 @@
+class Solution {
+    void nextPermutation(int[] arr) {
+        int pivot = -1;
+        for(int i = arr.length-1 ; i>0 ; i--){
+            if(arr[i]>arr[i-1]){
+                pivot = i-1;
+                break;
+            }
+        }
+        if(pivot == -1){
+            Arrays.sort(arr,0,arr.length);
+        }
+        else{
+            int repl = pivot+1;
+            for(int i = pivot+2 ; i<arr.length ; i++){
+                if(arr[i]>arr[pivot] && arr[i]<arr[repl]){
+                    repl = i;
+                }
+            }
+            arr[pivot] = (arr[pivot]+arr[repl]) - (arr[repl] = arr[pivot]);
+            Arrays.sort(arr,pivot+1,arr.length);
+        }
+        
+    }
+}
