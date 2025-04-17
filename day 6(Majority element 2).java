17367bc37a213c278b6a5cf8655797cd555bc46e
Ayush Sharma <116947667+ayushsharma00001@users.noreply.github.com>
2025-04-17 19:40:23 +0530

    Day 6

---
+++ day 6(Majority element 2).java
@@ -0,0 +1,17 @@
+class Solution {
+    // Function to find the majority elements in the array
+    public List<Integer> findMajority(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        List<Integer> l1 = new ArrayList<>();
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue() > nums.length/3){
+                l1.add(entry.getKey());
+            }
+        }
+        Collections.sort(l1);
+        return l1;
+    }
+}
