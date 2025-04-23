95b262a48b48ead5000e85bbfdbf2fe0749d6691
Ayush Sharma <116947667+ayushsharma00001@users.noreply.github.com>
2025-04-23 21:48:18 +0530

    Day 11

--- day 11(maximum product sub array).java
+++ day 11(maximum product sub array).java
@@ -1,16 +1,14 @@
 class Solution {
     // Function to find maximum product subarray
     int maxProduct(int[] arr) {
-        int maxProduct = arr[0];
-        int currMax = arr[0];
-        int currMin = arr[0];
-        for(int i = 1 ; i < arr.length ; i++){
-            if(arr[i]<0){
-                currMax = currMax+currMin - (currMin = currMax);
-            }
-            currMax = Math.max(arr[i],arr[i]*currMax);
-            currMin = Math.min(arr[i],arr[i]*currMin);
-            maxProduct = Math.max(maxProduct,currMax);
+        int maxProduct = Integer.MIN_VALUE;
+        int productPre = 1, productSuff = 1;
+        for(int i = 0 ; i < arr.length ; i++){
+            if(productPre == 0) productPre = 1;
+            if(productSuff == 0) productSuff = 1;
+            productPre = productPre*arr[i];
+            productSuff = productSuff*arr[arr.length-i-1];
+            maxProduct = Math.max(maxProduct, Math.max(productPre,productSuff));
         }
         return maxProduct;
         
