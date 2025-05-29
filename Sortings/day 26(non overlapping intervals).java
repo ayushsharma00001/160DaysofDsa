6542218a57b641a9a8532c2956f0d4762bee7eb8
Ayush Sharma <116947667+ayushsharma00001@users.noreply.github.com>
2025-05-29 21:31:53 +0530

    Day 26

---
+++ Sortings/day 26(non overlapping intervals).java
@@ -0,0 +1,14 @@
+class Solution {
+    static int minRemoval(int intervals[][]) {
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[1],b[1]));
+        int count = 0;
+        int prevEnd = Integer.MIN_VALUE;
+        for(int interval[]:intervals){
+            if(interval[0]>=prevEnd)
+                prevEnd = interval[1];
+            else
+                count++;
+        }
+        return count;
+    }
+}
